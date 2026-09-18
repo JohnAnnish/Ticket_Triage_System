67d95938331216edd3f4537337a6c63a7fd8373f
JohnAnnish <johnannish1234@gmail.com>
2026-09-19 04:20:26 +0530

    docs: add future export comments in utilities

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,2 +1,3 @@
 export { testRedisConnection } from "./redis/connectRedis.js";
 export { testSupabaseConnection } from "./supabase/connectSupabase.js";
+// Additional utility exports can be added here in the future
